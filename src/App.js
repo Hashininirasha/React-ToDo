461175dd4c49e5e26541406728f8e96118105518
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-06 16:50:01 +0530

    first text

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ class App extends React.Component{
     return(
       <div className="App">
         <header>
-          <form id="to-do app">
+          <form id="to-do-form">
             <input type="text" placeholder="Enter text"/>
             <button type="button" class="btn btn-warning">Add</button>
           </form>
         </header>
-      </div> 
+        </div>
     );
   }
 }
