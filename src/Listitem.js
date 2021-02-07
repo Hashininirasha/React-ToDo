4bb960484085d7d48a7cf41c9a11ea464dfa7c0f
Hashininirasha <hashininirashamunasinghe@gmail.com>
2021-02-07 20:16:50 +0530

    added fonts

--- src/Listitem.js
+++ src/Listitem.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import './Listitem.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 function Listitem(props){
     const items = props.items;
     const listitem = items.map((item) =>
         {
             return <div className="list" key={item.key}>
-                <p>{item.text}</p>
+                <p>{item.text}
+                <span>
+       
+                <FontAwesomeIcon className="faicons" icon="trash"/>
+                </span>
+                </p>
             </div>
         });
     return(
